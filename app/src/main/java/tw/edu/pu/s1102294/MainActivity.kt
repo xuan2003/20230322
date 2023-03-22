157c1ee2d46c102be03541354ba322d5bfa62c01
lyx <s1102294@gm.pu.edu.tw>
2023-03-22 11:08:08 +0800

    加入樂透功能

--- app/src/main/java/tw/edu/pu/s1102294/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/s1102294/MainActivity.kt
@@ -2,6 +2,7 @@ package tw.edu.pu.s1102294
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +11,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         var txv: TextView = findViewById(R.id.txv)
         txv.text = "0"
+    }
+    fun happy(v: View) {
+        var txv: TextView = findViewById(R.id.txv)
+        txv.text = (1..100).random().toString()
     }
 }
